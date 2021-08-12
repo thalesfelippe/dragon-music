c0f0a8b17941622157529b5695503232845bac35
Your Name <thales_so@hotmail.com>
2021-08-12 16:00:50 -0300

    projeto terminado

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
     <header>
+      <i class="fas fa-dragon"></i>
       <h1>Music</h1>
     </header>
     <main>
       <section class="player">
-        <h2 class="song-tittle">{{ current.title }} - <span>{{ current.artist }}</span></h2>
-        <div class="control">
-          <button class="prev">Prev</button>
-          <button class="play" v-if="!isPlaying" @click="play">Play</button>
-          <button class="pause" v-else @click="pause">Pause</button>
-          <button class="next">Next</button>
+        <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+        <div class="controls">
+          <button class="prev" @click="prev"><i class="fas fa-backward"></i></button>
+          <button class="play" v-if="!isPlaying" @click="play"><i class="fas fa-play"></i></button>
+          <button class="pause" v-else @click="pause"><i class="fas fa-pause"></i></button>
+          <button class="next" @click="next"><i class="fas fa-forward"></i></button>
         </div>
       </section>
       <section class="playlist">
@@ -75,11 +76,38 @@ export default {
       }
 
       this.player.play();
+      this.player.addEventListener('ended', function () {
+        this.index++;
+          if (this.index > this.songs.length -1 ){
+            this.index = 0;
+          }
+
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      }.bind(this));
       this.isPlaying = true;
     },
     pause () {
       this.player.pause();
       this.isPlaying = false;
+    },
+    next () {
+      this.index++;
+      if (this.index > this.songs.length -1 ){
+        this.index = 0;
+      }
+
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev () {
+      this.index--;
+      if (this.index < 0 ){
+        this.index = this.songs.length -1;
+      }
+
+      this.current = this.songs[this.index];
+      this.play(this.current);
     }
   },
   created () {
@@ -101,12 +129,120 @@ body {
   font-family: sans-serif;
 }
 
+#app {
+  background-color: #C0BABC;
+}
+
 header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 15px;
+  background-color: #011627;
+  color: #FDFFFC;
+}
+
+header i {
+  font-size: 50px;
+  margin-top: 10px;
+  color: #F71735;
+}
+
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
+  background-color: #E5D4CE;
+}
+
+.song-title {
+  color: #212121;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+
+.controls {
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 30px 15px;
+}
+
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+
+.play, .pause {
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: #FDFFFC;
+  background-color: #F71735;
+  transition: 0.4s;
+}
+
+button:hover {
+  opacity: 0.9;
+}
+
+.next, .prev {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: #FDFFFC;
+  background-color: #BA1B1D;
+  transition: 0.4s;
+}
+
+.playlist {
+  padding: 0px 30px;
+}
+
+.playlist h3 {
+  color: #011627;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+.playlist .song {
+  display: block;
+  width: 100%;
   padding: 15px;
-  background-color: #212121;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.playlist .song:hover {
+  color: #F71735;
+}
+
+.playlist .song.playing {
   color: #FFF;
+  background-image: linear-gradient(to right, #BA1B1D, #F71735);
+  border-radius: 20px;
+  width: 70%;
+  margin: 0 auto;
 }
 </style>
