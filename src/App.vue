e4d765b2c6e29d8c5f283b6a44aaea090db6bad4
Your Name <thales_so@hotmail.com>
2021-08-12 15:04:43 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,19 @@
     </header>
     <main>
       <section class="player">
-        <h2 class="song-tittle">{{ current.title }}</h2>
-        {{ songs[0].src }}
+        <h2 class="song-tittle">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+        <div class="control">
+          <button class="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next">Next</button>
+        </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
+          {{ song.title }} - {{ song.artist }}
+        </button>
       </section>
     </main>
   </div>
@@ -17,9 +28,9 @@ export default {
   name: 'App',
   data () {
     return {
-      current: {
-        title: 'SONG TITTLE'
-      },
+      current: {},
+      index: 0,
+      isPlaying: false,
       songs: [
         {
           title: 'Fight Back',
@@ -51,8 +62,30 @@ export default {
           artist: 'Neffex',
           src: require('./assets/neffex-soldier.mp3')
         }
-      ]
+      ],
+      player: new Audio()
     }
+  },
+  methods: {
+    play (song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+
+        this.player.src = this.current.src;
+      }
+
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
+    }
+  },
+  created () {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+    this.player.play();
   }
 }
 </script>
